16de38644bccc01b3c9fdb5ad33f4d1804e96e89
Thomas Birke <quafzi@netextreme.de>
2012-10-24 22:48:43 +0200

    write your code

---
+++ src/Quafzi/Weather.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Quafzi;
+
+class Weather
+{
+    private $clouds;
+    private $temperature;
+    private $rain;
+
+    public function parseInput($input)
+    {
+        if ($xml = @simplexml_load_string($input)) {
+            $this->clouds      = (int) $xml->clouds;
+            $this->temperature = (int) $xml->temperature;
+            $this->rain        = (int) $xml->rain;
+        } else {
+            throw new \Exception('service currently not available');
+        }
+    }
+
+    public function setClouds($clouds)
+    {
+        $this->clouds = $clouds;
+    }
+
+    public function setTemperature($temperature)
+    {
+        $this->temperature = $temperature;
+    }
+
+    public function setRain($rain)
+    {
+        $this->rain = $rain;
+    }
+
+    public function getCloudiness()
+    {
+        $cloudiness = 'bright';
+        if (10 <= $this->clouds) {
+            $cloudiness = 'partly cloudy';
+        }
+        if (30 <= $this->clouds) {
+            $cloudiness = 'cloudy';
+        }
+        if (75 <= $this->clouds) {
+            $cloudiness = 'clouded';
+        }
+        return $cloudiness;
+    }
+
+    public function getSummary()
+    {
+        if (80 < $this->clouds
+            && $this->temperature < 20
+            && 0 < $this->rain
+        ) {
+            return 'shit weather';
+        }
+        $summary = sprintf(
+            'a %s %s day',
+            (0 == $this->rain && $this->clouds < 50) ? 'beautiful' : 'ugly',
+            20 < $this->temperature ? 'warm' : 'cold'
+        );
+        return $summary;
+    }
+}
