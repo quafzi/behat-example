22bfe5bb3b75cb53e271aedd4d0b99e25e7b4473
Thomas Birke <tbirke@netextreme.de>
2013-04-09 11:33:24 +0200

    increase unit test coverage

--- tests/Quafzi/WeatherTest.php
+++ tests/Quafzi/WeatherTest.php
@@ -2,10 +2,45 @@
 namespace QuafziTest;
 
 require_once 'src/Quafzi/Weather.php';
+require_once 'src/Quafzi/Service.php';
 use \Quafzi\Weather;
+use \Mockery as m;
 
 class WeatherTest extends \PHPUnit_Framework_TestCase
 {
+    public function testFetchData()
+    {
+        // we need to mock our Service
+        // using PHPUnit
+        $service = $this->getMock('\Quafzi\Service', array('getData'));
+        $service->expects($this->any())
+            ->method('getData')
+            ->will($this->returnValue($this->getInput(0, -10, 0)));
+
+        /*
+        // using Mockery
+        $service = m::mock('\Quafzi\Service');
+        $service->shouldReceive('getData')
+            ->andReturn($this->getInput(0, -10, 0));
+        */
+
+        $weather = new Weather();
+        $weather->fetchData($service);
+        $this->assertEquals(0, $weather->getClouds());
+        $this->assertEquals(-10, $weather->getTemperature());
+        $this->assertEquals(0, $weather->getRain());
+    }
+
+    public function testMagicGetter()
+    {
+        $weather = new Weather();
+        $this->assertNull($weather->getClouds());
+        $this->assertNull($weather->getTemperature());
+        $this->assertNull($weather->getRain());
+        $this->setExpectedException('BadMethodCallException');
+        $this->assertNull($weather->getFog());
+    }
+
     public function testParseInputValid()
     {
         $weather = new Weather();
@@ -37,6 +72,11 @@ class WeatherTest extends \PHPUnit_Framework_TestCase
         $weather->setTemperature(27);
         $weather->setRain(0);
         $this->assertEquals('a beautiful warm day', $weather->getSummary());
+
+        $weather->setClouds(81);
+        $weather->setTemperature(7);
+        $weather->setRain(1);
+        $this->assertEquals('shit weather', $weather->getSummary());
     }
 
     public function testParseInputInvalid()
@@ -67,5 +107,9 @@ class WeatherTest extends \PHPUnit_Framework_TestCase
 </weather>
 XML;
         return sprintf($input, $clouds, $rain, $temperature);
+    }
+
+    public function tearDown() {
+        m::close();
     }
 }
